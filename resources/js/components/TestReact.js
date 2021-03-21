4ca7d6e7fd056c6146feed628c19dad9d9cd9fed
Roman <roman82direct@yandex.ru>
2021-03-21 15:12:37 +0300

    add React

---
+++ resources/js/components/TestReact.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+/* An example React component */
+class TestReact extends Component {
+    constructor() {
+        super();
+        this.state = {
+            users: [],
+        }
+    }
+    /*componentDidMount() is a lifecycle method
+     * that gets called after the component is rendered
+     */
+    componentDidMount() {
+        /* fetch API in action */
+        fetch('/users')
+            .then(response => {
+                return response.json();
+            })
+            .then(users => {
+                //Fetched item is stored in the state
+                this.setState({ users });
+            });
+    }
+
+    renderUsers() {
+        return this.state.users.map(user => {
+            return (
+                /* When using list you need to specify a key
+                 * attribute that is unique for each list item
+                */
+                <div key={user.id}>
+                    <p>{user.id}</p><br/>
+                    <strong>{ user.name }</strong><br/>
+                    { user.email}
+                    <hr/>
+                </div>
+            );
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>All Users</h3>
+                <ul>
+                    { this.renderUsers() }
+                </ul>
+            </div>
+
+        );
+    }
+}
+
+export default TestReact;
+
+if (document.getElementById('users')) {
+    ReactDOM.render(<TestReact />, document.getElementById('users'));
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" crossorigin="anonymous">
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
+
+            <!-- Page Heading -->
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+{{--           "Без этого колхоза Бутстрап не работает--}}
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    </body>
+</html>
