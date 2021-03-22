d442859ad6bfdb4b3da74391f39999532d9ebd18
Roman <roman82direct@yandex.ru>
2021-03-22 21:36:05 +0300

    done

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function getAllUsers(){
+//        dd(User::all()->toJson());
+        return User::all();
+    }
+    public function getCurrentUser(){
+        return Auth::user();
+    }
+    public function getUserById($user_id){
+        return User::find($user_id);
+    }
+}
